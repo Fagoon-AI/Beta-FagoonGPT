46a232c21389270c93036d12f52181f759665f45
Gyaneshwar Sah <gyaneshwar585@gmail.com>
2024-03-15 08:55:34 +0545

    responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,22 +15,22 @@ export default function Home() {
     <main className="flex flex-col py-7 justify-between h-screen">
       <div className="flex flex-col gap-8 overflow-y-auto scrollbar-none mb-4">
         <div className="flex flex-col gap-8">
-          <h1 className="text-2xl bg-gradient-to-br from-[#B664DB] via-[#DDDDDD] to-[#FF3F9B] bg-clip-text text-transparent">
+          <h1 className="md:text-2xl text-xl bg-gradient-to-br from-[#B664DB] via-[#DDDDDD] to-[#FF3F9B] bg-clip-text text-transparent">
             Fagoon
           </h1>
           <div className="flex flex-col gap-1">
-            <h1 className="text-5xl bg-gradient-to-r from-[#4285F4] via-[#9B72CB] via-[#D96570] via-[#D96570] via-[#9B72CB] via-[#4285F4] via-[#9B72CB] via-[#D96570] to-[#131314] bg-clip-text text-transparent">
+            <h1 className="md:text-5xl text-2xl bg-gradient-to-r from-[#4285F4] via-[#9B72CB] via-[#D96570] via-[#D96570] via-[#9B72CB] via-[#4285F4] via-[#9B72CB] via-[#D96570] to-[#131314] bg-clip-text text-transparent">
               Hello, this is FagoonGPT
             </h1>
-            <h2 className="text-5xl">How can I help you today?</h2>
+            <h2 className="md:text-5xl text-2xl">How can I help you today?</h2>
           </div>
         </div>
         <div className="flex flex-col gap-5">
-          <span className="text-xl">You Might Want To Try</span>
+          <span className="md:text-xl text-lg">You Might Want To Try</span>
           <div className="flex overflow-x-auto max-w-full scrollbar-none">
             <div className="flex items-center justify-between gap-3">
               <TextBox label="Write a poem">
-                <PoemIcon />
+                <PoemIcon width="40" height="40" />
               </TextBox>
               <TextBox label="Translate anything">
                 <TranslateIcon />
@@ -60,7 +60,7 @@ export default function Home() {
           </div>
         </div>
         <div className="flex flex-col gap-5">
-          <h2 className="text-xl">Recent</h2>
+          <h2 className="md:text-xl text-lg">Recent</h2>
           <div className="flex flex-col gap-3">
             <TextBox label="Job finder UX"></TextBox>
             <TextBox label="Graphic design copy"></TextBox>
---
+++ src/types/IconProps.ts
@@ -0,0 +1,5 @@
+export interface IconProps {
+  classname?: string;
+  width: string;
+  height: string;
+}
